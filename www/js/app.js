70876f7ecec90e16df49531b24294e1bd6547507
Nikhil Sharma <nikhil.sharma.it@gmail.com>
2014-03-03 00:50:49 +0530

    project changed

--- www/js/app.js
+++ www/js/app.js
@@ -1,45 +1,15 @@
 'use strict';
-
-angular.module('AppD', [
+var myApp = angular.module('starter', [
     'PhoneGap',
     'LocalStorageModule',
-    'ionic', 
-    'starter.services', 
-    'starter.controllers'
-]).config(function($stateProvider, $urlRouterProvider) {
+    'ionic'
+]);
+myApp.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
-
-    // setup an abstract state for the tabs directive
-    .state('tab', {
-      url: "/tab",
-      abstract: true,
-      templateUrl: "templates/tabs.html"
-    })
-
-    // the pet tab has its own child nav-view and history
-    .state('tab.pet-index', {
-      url: '/pets',
-      views: {
-        'pets-tab': {
-          templateUrl: 'templates/pet-index.html',
-          controller: 'PetIndexCtrl'
-        }
-      }
-    })
-    $routeProvider
-            .when('/', {
-                templateUrl: 'partials/applicationPage.html',
-                controller: 'ApplicationController'})
-            .when('/profile', {
-                templateUrl: 'partials/profileScreen.html',
-                controller: 'ProfileController'})
-            .when('/details', {
-                templateUrl: 'partials/detailsScreen.html',
-                controller: 'DetailsController'})
-            .when('/login', {
-                templateUrl: 'partials/loginScreen.html',
-                controller: 'LoginController'})
-            .otherwise({
-                redirectTo: '/'
-            });
+            .state('home', {
+                url: '/',
+                templateUrl: 'partials/home.html',
+                controller: 'HomeController'});
+    $urlRouterProvider.otherwise("/");
 });
+
